16a74d75af76e2307dc989f8b05bfb33555d9763
matheuscarvalheira <matheuscarvalheiraf@gmail.com>
2020-12-02 21:14:50 -0300

    Fix: Function hash password fixed

--- src/controllers/hashPassword.js
+++ src/controllers/hashPassword.js
@@ -1,12 +1,14 @@
 const bcrypt = require("bcrypt");
 
-async function hashPassword(password) {
-  try {
-    const hash = await bcrypt.hash(password, 10);
-    return hash;
-  } catch (error) {
-    console.log(error.message);
+module.exports = {
+  async hashPassword(password) {
+    try {
+      const hash = await bcrypt.hash(password, 10);
+      return hash;
+    } catch (error) {
+      console.log(error.message);
+    }
   }
 }
 
-module.exports = hashPassword;
+
